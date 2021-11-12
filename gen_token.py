ba464ba78aa0d785377826bb68cda42bf7271d38
张锦池 <zhangjinchi@zhangjinchis-MacBook-Pro.local>
2021-11-12 18:40:34 -0500

    change colab notebook

--- gen_token.py
+++ gen_token.py
@@ -24,4 +24,4 @@ f.close()
 tokenizer.train(files=paths, vocab_size=52000, min_frequency=2, special_tokens=special_tokens)
 
 # Save files to disk
-tokenizer.save_model(".", "poem")
+tokenizer.save_model("poem")
